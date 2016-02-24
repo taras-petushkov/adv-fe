460bf5a0a969b9c288ee654dd387c17e5c679e20
Taras Petushkov <taras.petushkov+main@gmail.com>
2016-02-24 17:25:26 +0200

    Lesson 3| review all css classes after BEM intro, split all big css to the small ones

--- src/post.js
+++ src/post.js
@@ -3,12 +3,12 @@ $(document).ready(function () {
     var comments = Data.getPostComments();
     var relatedPosts = Data.getRelatedPosts();
 
-    var postTemplate = Handlebars.compile($('#selected-post-template').html());
-    var relatedPostsTemplate = Handlebars.compile($('#related-posts__list-template').html());
+    var postTemplate = Handlebars.compile($('#post-template').html());
+    var relatedPostsTemplate = Handlebars.compile($('#related-posts-template').html());
 
     Handlebars.registerPartial('comment', $('#comment-template').html());
     Handlebars.registerPartial('comments', $('#comments-template').html());
-    Handlebars.registerPartial('post-preview', $('#post-template').html());
+    Handlebars.registerPartial('post-preview', $('#post-preview-template').html());
 
 
     render();
@@ -20,18 +20,18 @@ $(document).ready(function () {
     }
 
     function renderPost() {
-        $('.selected-post').html(postTemplate({
+        $('.post').html(postTemplate({
             post: currentPost,
             comments: comments
         }));
     }
 
     function renderRelatedPosts() {
-        $('.related-posts__list').html(relatedPostsTemplate({posts: relatedPosts}));
+        $('.related-posts').html(relatedPostsTemplate({posts: relatedPosts}));
     }
 
     function subscribeHandlers() {
-        $('.related-posts__list').click(function (event) {
+        $('.related-posts').click(function (event) {
             var id = $(event.target).closest('.post-preview').data('id');
             if (id === undefined) {
                 return;
